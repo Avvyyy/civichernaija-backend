9f11ab67d53b4c99ba4871facb11e720582bb96a
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2026-04-05 23:13:17 +0100

    Updated mongoUrl config

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/opportunities', opportunityRoutes);
 
 // Database connection
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/civichernaija';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
